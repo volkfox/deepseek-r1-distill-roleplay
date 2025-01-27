6735d1c3b71cf9a7e7703c9b7c6fe8f51043b395
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-01-27 14:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class ResponseObjects(BaseModel):
 
 class InferlessPythonModel:
   def initialize(self):
-    model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
+    model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
     self.llm = LLM(model=model_id,gpu_memory_utilization=0.9,max_model_len=5000)
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
