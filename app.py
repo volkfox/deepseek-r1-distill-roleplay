f37c1c004c17a13302c0e0d0e04723d0ba73ef74
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-02-04 02:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
 from transformers import AutoTokenizer
-import inferless
-from pydantic import BaseModel, Field
-from typing import Optional
-
-@inferless.request
-class RequestObjects(BaseModel):
-  prompt: str = Field(default="Explain Deep Learning.")
-  temperature: Optional[float] = 0.7
-  top_p: Optional[float] = 0.1
-  repetition_penalty: Optional[float] = 1.18
-  top_k: Optional[int] = 40
-  max_tokens: Optional[int] = 256
-
-@inferless.response
-class ResponseObjects(BaseModel):
-  generated_text: str = Field(default='Test output')
 
 class InferlessPythonModel:
   def initialize(self):
@@ -24,15 +8,23 @@ class InferlessPythonModel:
     self.llm = LLM(model=model_id,gpu_memory_utilization=0.9,max_model_len=5000,dtype="float16")
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-  def infer(self, request: RequestObjects) -> ResponseObjects:
-    sampling_params = SamplingParams(temperature=request.temperature,top_p=request.top_p,repetition_penalty=request.repetition_penalty,
-                     top_k=request.top_k,max_tokens=request.max_tokens)
-    input_text = self.tokenizer.apply_chat_template([{"role": "user", "content": request.prompt}], tokenize=False)
+  def infer(self, inputs):
+    prompts = inputs["prompt"]
+    temperature = inputs.get("temperature",0.7)
+    top_p = inputs.get("top_p",0.1)
+    repetition_penalty = inputs.get("repetition_penalty",1.18)
+    top_k = int(inputs.get("top_k",40))
+    max_tokens = inputs.get("max_tokens",256)
+
+    sampling_params = SamplingParams(temperature=temperature,top_p=top_p,
+                                     repetition_penalty=repetition_penalty,
+                                     top_k=top_k,max_tokens=max_tokens
+                                    )
+    input_text = self.tokenizer.apply_chat_template([{"role": "user", "content": prompts}], tokenize=False)
     result = self.llm.generate(input_text, sampling_params)
     result_output = [output.outputs[0].text for output in result]
 
-    generateObject = ResponseObjects(generated_text = result_output[0])        
-    return generateObject
+    return {"generated_text":result_output[0]}
 
   def finalize(self):
     self.llm = None
